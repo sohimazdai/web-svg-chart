547730fdd994e92a7459ce7de630a596ffbf0256
Andrey Pankratov <i@doq.su>
2019-04-06 10:16:33 +0300

    #1-1: add inputs

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { UserInputBlock } from '../components/UserInputBlock';
+
+export class MainPage extends Component {
+  render() {
+    return <div>
+      <p>There is MainPage</p>
+      <UserInputBlock />
+    </div>
+  }
+}
