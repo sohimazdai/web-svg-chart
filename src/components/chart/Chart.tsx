75c64f70e9200cdb81813102f8fe9357794ab8f2
Andrey Pankratov <i@doq.su>
2019-04-13 11:47:30 +0300

    #8-5: add simple glucose chart
    finalmente

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Note } from '../../interfaces/Notes';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import './Chart.css';
 
 export interface ChartProps {
@@ -8,7 +9,17 @@ export interface ChartProps {
 
 export class Chart extends React.Component<ChartProps> {
 
+
+private renderLineChart = (
+  <LineChart width={document.body.offsetWidth*0.7} height={400} data={this.props.notes.slice(0, 10)}>
+    <Line type="monotone" dataKey="glucose" stroke="tomato" />
+    <CartesianGrid stroke="yellow" strokeDasharray="5 5" />
+    <XAxis />
+    <YAxis dataKey="glucose"/>
+    <Tooltip />
+  </LineChart>
+);
     render() {
-        return <div className={'chart'}></div>
+        return <div className={'chart'}>{this.renderLineChart}</div>
     }
 }
